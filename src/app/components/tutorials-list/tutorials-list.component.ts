5902a5eef018bec996e702eda5b98b71d8c7e518
Nkululeko <nkululekob.maseko@gmail.com>
2024-04-07 16:25:48 +0200

    Feature: Implementing Tutorials list component

--- src/app/components/tutorials-list/tutorials-list.component.ts
+++ src/app/components/tutorials-list/tutorials-list.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Tutorial} from "../../models/tutorial.model";
+import {TutorialService} from "../../services/tutorial.service";
+import {map} from "rxjs";
 
 @Component({
   selector: 'app-tutorials-list',
   templateUrl: './tutorials-list.component.html',
   styleUrl: './tutorials-list.component.css'
 })
-export class TutorialsListComponent {
+export class TutorialsListComponent implements OnInit {
+  tutorials?: Tutorial[];
+  currentTutorial?: Tutorial;
+  currentIndex = -1;
+  title = '';
 
+  constructor(private tutorialService: TutorialService) {
+  }
+
+  ngOnInit(): void {
+    this.retrieveTutorials();
+  }
+
+  refreshList(): void {
+    this.currentTutorial = undefined;
+    this.currentIndex = -1;
+    this.retrieveTutorials();
+  }
+
+  retrieveTutorials(): void {
+    this.tutorialService.getAll().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({
+            id: c.payload.doc.id, ...c.payload.doc.data()
+          })
+        )
+      )
+    ).subscribe(data => {
+      this.tutorials = data;
+    });
+  }
+
+  setActiveTutorial(tutorial: Tutorial, index: number): void {
+    this.currentTutorial = tutorial;
+    this.currentIndex = index;
+  }
 }
