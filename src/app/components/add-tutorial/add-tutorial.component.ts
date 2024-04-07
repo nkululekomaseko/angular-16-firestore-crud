35365add61e417dbe75c11919d81c49541ae4292
Nkululeko <nkululekob.maseko@gmail.com>
2024-04-07 16:06:27 +0200

    Feature: Implementing Add tutorial component

--- src/app/components/add-tutorial/add-tutorial.component.ts
+++ src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Tutorial} from "../../models/tutorial.model";
+import {TutorialService} from "../../services/tutorial.service";
 
 @Component({
   selector: 'app-add-tutorial',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './add-tutorial.component.css'
 })
 export class AddTutorialComponent {
+  tutorial: Tutorial = new Tutorial();
+  submitted = false;
 
+  constructor(private tutorialService: TutorialService) {
+  }
+
+  saveTutorial(): void {
+    this.tutorialService.create(this.tutorial).then(() => {
+      console.log("Created new tutorial successfully!");
+      this.submitted = true;
+    })
+  }
+
+  newTutorial(): void {
+    this.submitted = false;
+    this.tutorial = new Tutorial();
+  }
 }
