186913ae1ba7cfa87ac93fa91d876adbc57942e3
Nkululeko <nkululekob.maseko@gmail.com>
2024-04-07 15:37:54 +0200

    Feature: Configuring Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  isProduction: false,
+  firebase: {
+    apiKey: "AIzaSyBK3TOHN_G2Ts9crI_GwRXorYoqMFqhpS0",
+    authDomain: "nk-angular-firebase.firebaseapp.com",
+    projectId: "nk-angular-firebase",
+    storageBucket: "nk-angular-firebase.appspot.com",
+    messagingSenderId: "436438747836",
+    appId: "1:436438747836:web:8ac57005b59eaa4aeb5eb9",
+    measurementId: "G-RZL3CTLBK9"
+  }
+};
