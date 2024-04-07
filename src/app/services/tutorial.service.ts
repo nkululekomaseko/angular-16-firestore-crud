1cce09e846d3d1d1d47ef467367a534cf3eb102c
Nkululeko <nkululekob.maseko@gmail.com>
2024-04-07 15:46:52 +0200

    Feature: Implementing tutorial service

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, DocumentReference} from "@angular/fire/compat/firestore";
+import {Tutorial} from "../models/tutorial.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TutorialService {
+  tutorialsRef: AngularFirestoreCollection<Tutorial>;
+  private dbPath = '/tutorials';
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {
+    this.tutorialsRef = db.collection(this.dbPath);
+  }
+
+  getAll(): AngularFirestoreCollection<Tutorial> {
+    return this.tutorialsRef;
+  }
+
+  create(tutorial: Tutorial): Promise<DocumentReference<Tutorial>> {
+    return this.tutorialsRef.add(tutorial);
+  }
+
+  update(id: string, partialTutorial: Partial<Tutorial>): Promise<void> {
+    return this.tutorialsRef.doc(id).update(partialTutorial);
+  }
+
+  delete(id: string): Promise<void> {
+    return this.tutorialsRef.doc(id).delete();
+  }
 }
